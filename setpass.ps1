6ee21e38e9c4da84bcfe01d33d2d0e938cd2ce1d
nsandeepprotech <61390573+nsandeepprotech@users.noreply.github.com>
2021-05-11 22:00:38 +0530

    Update setpass.ps1

--- setpass.ps1
+++ setpass.ps1
@@ -1,4 +1,4 @@
 #$Password = "kumar"
 #$UserAccount = Get-LocalUser -Name "runneradmin"
 #$UserAccount | Set-LocalUser -Password $Password
-net user "runneradmin" "sandy"
+net user "runneradmin" "GUn701-752hgf-88dN8G-74iLs"
