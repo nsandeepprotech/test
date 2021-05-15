5bf9647c649f62c85d79f56171b4efcab23f078b
nsandeepprotech <61390573+nsandeepprotech@users.noreply.github.com>
2021-05-15 15:20:40 +0530

    Update set_permssions_for_publickey.ps1

--- set_permssions_for_publickey.ps1
+++ set_permssions_for_publickey.ps1
@@ -1,4 +1,5 @@
-$acl = Get-Acl C:\ProgramData\ssh\administrators_authorized_keys
+
+$acl = Get-Acl C:\users\runneradmin\.ssh\authorized_keys
 $acl.SetAccessRuleProtection($true, $false)
 $administratorsRule = New-Object system.security.accesscontrol.filesystemaccessrule("Administrators","FullControl","Allow")
 $systemRule = New-Object system.security.accesscontrol.filesystemaccessrule("SYSTEM","FullControl","Allow")
